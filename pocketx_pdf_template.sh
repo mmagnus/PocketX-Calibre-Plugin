a052025cb208c90f0c0d25113376430b2c66826c
Marcin Magnus <mag_dex@o2.pl>
2021-03-11 11:35:48 +0100

    Add a new variants of PocketX

---
+++ pocketx_pdf_template.sh
@@ -0,0 +1,3 @@
+# --verbose 
+ ebook-convert PocketX_FixTitleAutoTags.recipe .pdf --debug-pipeline debug --base-font-size 9 --change-justification "justify"
+mv -v PocketX_FixTitleAutoTags.pdf "/Users/magnus/Dropbox/boox/PocketX `date +'%b %d %Y'`.pdf"
